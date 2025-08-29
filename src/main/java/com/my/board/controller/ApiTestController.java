eeede1de1e6c0f428ab9e0da96c2e788d8ed8b47
noeyos-p <krapnoeyos@gmail.com>
2025-08-29 17:13:18 +0900

    깃에 올려요 apitest

---
+++ src/main/java/com/my/board/controller/ApiTestController.java
@@ -0,0 +1,18 @@
+package com.my.board.controller;
+
+import com.my.board.dto.LoginDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApiTestController {
+    @PostMapping("/apiTest")
+    @ResponseBody
+    public LoginDto getTest() {
+        return new LoginDto("noeyos", "1111");
+    }
+}
+
